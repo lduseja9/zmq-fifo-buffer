52912b65659211a863335e9416c8d4d9bc181647
Lalit Duseja <lalitduseja9@gmail.com>
2026-05-20 15:30:33 -0500

    Implementation of mutex-free fifo buffer server using ZeroMq

---
+++ server/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	pb "github.com/lduseja9/zmq-fifo-buffer/common/proto"
+	"github.com/lduseja9/zmq-fifo-buffer/server/storage"
+
+	zmq "github.com/go-zeromq/zmq4"
+	"google.golang.org/protobuf/proto"
+)
+
+const endpoint = "tcp://127.0.0.1:5555"
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	fifoBuffer := storage.NewFifoBuffer()
+	defer fifoBuffer.Close()
+	go fifoBuffer.Start()
+
+	// set up a ZeroMQ REP socket to listen for incoming messages
+	socket := zmq.NewRep(ctx)
+	defer socket.Close()
+
+	// Bind the socket to the specified endpoint
+	if err := socket.Listen(endpoint); err != nil {
+		fmt.Printf("Failed to listen on %s: %v\n", endpoint, err)
+		panic(err)
+	}
+	fmt.Println("FIFO server listening on: ", endpoint)
+
+	// Handle graceful shutdown on interrupt signals
+	// SIGINT - triggered by Ctrl+C
+	// SIGTERM - standard termination signal by OS or process managers like systemd, Docker or Kubernetes
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-stop
+		fmt.Println("Shutting down server...")
+		cancel()
+	}()
+
+	// Main loop to receive and respond to messages
+	for {
+		msg, err := socket.Recv()
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				fmt.Println("Server stopped.")
+				return
+			default:
+				fmt.Printf("Error receiving message: %v\n", err)
+				continue
+			}
+		}
+
+		fmt.Printf("Received message: %s\n", string(msg.Bytes()))
+
+		wireRequest := &pb.Request{}
+		if err := proto.Unmarshal(msg.Frames[0], wireRequest); err != nil {
+			sendResponse(socket, &pb.Response{Status: pb.Status_Failed})
+			continue
+		}
+
+		reponsePayload := requestHandler(fifoBuffer, wireRequest)
+		sendResponse(socket, reponsePayload)
+	}
+}
+
+func requestHandler(fifoBuffer *storage.FifoBuffer, request *pb.Request) *pb.Response {
+	fmt.Printf("Handling request: Operation=%s, Data=%q\n", request.Operation.String(), request.Data)
+
+	switch request.Operation {
+	case pb.Operation_PUSH_QUEUE:
+		// Here you would add the item to your queue
+		fmt.Printf("Pushing item to queue: %q\n", request.Data)
+		fifoBuffer.Push(request.Data)
+		return &pb.Response{Status: pb.Status_Success}
+	case pb.Operation_PULL_QUEUE:
+		// Here you would pull an item from your queue
+		result := fifoBuffer.Pull()
+		if result.Ok {
+			return &pb.Response{Status: pb.Status_Success, Data: result.Item}
+		} else {
+			return &pb.Response{Status: pb.Status_Empty}
+		}
+	case pb.Operation_SIZE_QUEUE:
+		// Here you would return the size of your queue
+		size := fifoBuffer.Size()
+		return &pb.Response{Status: pb.Status_Success, Size: int64(size)}
+	default:
+		fmt.Printf("Unknown operation: %s\n", request.Operation.String())
+		return &pb.Response{Status: pb.Status_Failed}
+	}
+
+}
+
+func sendResponse(socket zmq.Socket, response *pb.Response) {
+	wireResponse, err := proto.Marshal(response)
+	if err != nil {
+		fmt.Printf("Error marshaling response: %v\n", err)
+		return
+	}
+
+	if err := socket.Send(zmq.NewMsg(wireResponse)); err != nil {
+		fmt.Printf("Error sending response: %v\n", err)
+		return
+	}
+}
